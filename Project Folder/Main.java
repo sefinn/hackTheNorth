ba6dfbbac2ea0062e1850fdf5863b07789258990
Misha Wilkinson <mgkoudro@edu.uwaterloo.ca>
2018-09-15 18:02:09 -0400

    Extra cleanup

---
+++ Project Folder/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    Card testcard = new Card(2,2);
+    System.out.println(testcard.getSuit());
+
+  }
+}
